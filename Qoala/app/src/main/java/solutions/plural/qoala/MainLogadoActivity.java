7addb632ab2719708daf0381ae435a05748d06a8
Gabriel Lucas de Toledo Ribeiro <gabrielltr84@gmail.com>
2016-09-29 19:30:50 -0300

    added menu to mainLogado activity

--- Qoala/app/src/main/java/solutions/plural/qoala/MainLogadoActivity.java
+++ Qoala/app/src/main/java/solutions/plural/qoala/MainLogadoActivity.java
@@ -1,14 +1,16 @@
 package solutions.plural.qoala;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import solutions.plural.qoala.utils.SessionResources;
 
-public class MainLogadoActivity extends Activity {
+public class MainLogadoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +19,64 @@ public class MainLogadoActivity extends Activity {
 
         TextView txtUser = (TextView) findViewById(R.id.txtUser);
         txtUser.setText(SessionResources.getInstance().getUser().toString());
+
+        setupMenuBar();
     }
 
-    public void logoutClick(View v) {
+    /**
+     * Set up the {@link android.app.ActionBar}, if the API is available.
+     */
+    private void setupMenuBar() {
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        myToolbar.setTitle(R.string.app_name);
+        setSupportActionBar(myToolbar);
+
+    }
+
+    public void logout() {
         SessionResources.getInstance(true).setToken("", this);
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent i;
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                i = new Intent(this, SettingsActivity.class);
+                startActivity(i);
+                return true;
+
+            case R.id.action_mydevices:
+                i = new Intent(this, MainLogadoActivity.class);
+                startActivity(i);
+                return true;
+
+            case R.id.action_myprofile:
+                i = new Intent(this, MainLogadoActivity.class);
+                startActivity(i);
+                return true;
+
+            case R.id.action_logout:
+                logout();
+                i = new Intent(this, SplashActivity.class);
+                startActivity(i);
+                finish();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
